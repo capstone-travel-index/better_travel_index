fe8a415dcf62654c377f96f901ee2be5c4653e65
Michael <michael@Michi-PC-Mac.fritz.box>
2023-06-27 12:18:06 +0200

    update

---
+++ aus_amt_list.py
@@ -0,0 +1,52 @@
+# Auswärtige Amt:
+
+# Reise- und Sicherheitshinweise (Reisewarnung):
+#     Afghanistan
+#     Belarus
+#     Haiti
+#     Iran
+#     Jemen
+#     Libyen
+#     Myanmar
+#     Somalia
+#     Sudan
+#     Südsudan
+#     Syrien
+#     Ukraine
+#     Zentralafrikanische Republik
+
+# Reise- und Sicherheitshinweise (Teilreisewarnung):
+#     Ägypten
+#     Algerien
+#     Armenien
+#     Aserbaidschan
+#     Äthiopien
+#     Burkina Faso
+#     Côte d'Ivoire
+#     Demokratische Republik Kongo
+#     Eritrea
+#     Irak
+#     Japan
+#     Kamerun
+#     Libanon
+#     Mali
+#     Mauretanien
+#     Mosambik
+#     Nigeria
+#     Niger
+#     Pakistan
+#     Palästinensische Gebiete
+#     Philippinen
+#     Russische Föderation
+#     Tschad
+#     Venezuela
+
+# Einreise strikt untersagt:
+#     Demokratische Volksrepublik Korea (Nordkorea)
+
+
+reisewarnung = ['AFG', 'BLR', 'HTI', 'IRN', 'YEM', 'LBY', 'MMR', 'SOM', 'SDN', 'SSD', 'SYR', 'UKR', 'CAF']
+teilreisewarnung = ['EGY', 'DZA', 'ARM', 'AZE', 'ETH', 'BFA', 'CIV', 'COD', 'ERI', 'IRQ', 'JPN', 'CMR', 'LBN', 'MLI', 'MRT', 'MOZ', 'NGA', 'NER', 'PAK', 'PSE', 'PHL', 'RUS', 'TCD', 'VEN']
+untersagt = ['PRK']
+
+iso_droplist = ['AFG', 'BLR', 'HTI', 'IRN', 'YEM', 'LBY', 'MMR', 'SOM', 'SDN', 'SSD', 'SYR', 'UKR', 'CAF', 'PRK']
