8c609e30b5e80a1c1481bb1ae4d71ed7d470ab9c
Reto Burch <retoburch@MacBook-Air-von-Reto.local>
2023-06-30 16:54:50 +0200

    arrival and clen esgs

--- country_iso_dict.py
+++ country_iso_dict.py
@@ -506,3 +506,39 @@ country_iso_dict_ger = {
 }
 
 # pycountry-Bibliothek
+
+
+list_off = ['AFG', 'ALB', 'DZA', 'AND', 'AGO', 'ATG',
+            'ARG', 'ARM', 'AUS', 'AUT', 'AZE', 'BHS',
+            'BHR', 'BGD', 'BRB', 'BLR', 'BEL', 'BLZ',
+            'BEN', 'BTN', 'BOL', 'BIH', 'BWA', 'BRA',
+            'BRN', 'BGR', 'BFA', 'BDI', 'CPV', 'KHM',
+            'CMR', 'CAN', 'CAF', 'TCD', 'CHL', 'CHN',
+            'COL', 'COM', 'COG', 'COD', 'CRI', 'CIV',
+            'HRV', 'CUB', 'CYP', 'CZE', 'DNK', 'DJI',
+            'DMA', 'DOM', 'ECU', 'EGY', 'SLV', 'GNQ',
+            'ERI', 'EST', 'SWZ', 'ETH', 'FJI', 'FIN', 
+            'FRA', 'GAB', 'GMB', 'GEO', 'DEU', 'GHA',
+            'GRC', 'GRD', 'GTM', 'GIN', 'GNB', 'GUY',
+            'HTI', 'HND', 'HUN', 'ISL', 'IND', 'IDN',
+            'IRN', 'IRQ', 'IRL', 'ISR', 'ITA', 'JAM',
+            'JPN', 'JOR', 'KAZ', 'KEN', 'KIR', 'PRK',
+            'KOR', 'KWT', 'KGZ', 'LAO', 'LVA', 'LBN',
+            'LSO', 'LBR', 'LBY', 'LIE', 'LTU', 'LUX',
+            'MDG', 'MWI', 'MYS', 'MDV', 'MLI', 'MLT',
+            'MHL', 'MRT', 'MUS', 'MEX', 'FSM', 'MDA',
+            'MCO', 'MNG', 'MNE', 'MAR', 'MOZ', 'MMR',
+            'NAM', 'NRU', 'NPL', 'NLD', 'NZL', 'NIC',
+            'NER', 'NGA', 'MKD', 'NOR', 'OMN', 'PAK',
+            'PLW', 'PAN', 'PNG', 'PRY', 'PER', 'PHL',
+            'POL', 'PRT', 'QAT', 'ROU', 'RUS', 'RWA',
+            'KNA', 'LCA', 'VCT', 'WSM', 'SMR', 'STP',
+            'SAU', 'SEN', 'SRB', 'SYC', 'SLE', 'SGP',
+            'SVK', 'SVN', 'SLB', 'SOM', 'ZAF', 'SSD',
+            'ESP', 'LKA', 'SDN', 'SUR', 'SWE', 'CHE',
+            'SYR', 'TJK', 'TZA', 'THA', 'TLS', 'TGO',
+            'TON', 'TTO', 'TUN', 'TUR', 'TKM', 'TUV',
+            'UGA', 'UKR', 'ARE', 'GBR', 'USA', 'URY',
+            'UZB', 'VUT', 'VEN', 'VNM', 'YEM', 'ZMB',
+            'ZWE'
+]
